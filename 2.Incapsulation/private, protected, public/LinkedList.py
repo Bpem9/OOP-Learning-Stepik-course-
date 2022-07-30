e158e05d64d73e9934ea2a3b9e95535cb0ac909c
Bpem9 <itimeq@gmail.com>
2022-07-30 17:10:14 +0300

    Refactoring

--- 2.Incapsulation/private, protected, public/LinkedList.py
+++ 2.Incapsulation/private, protected, public/LinkedList.py
@@ -17,29 +17,7 @@ class LinkedList:
         return [obj.__data for obj in self.llst]
 
 
-class ObjList():
-    def __init__(self, data):
-        self.__data = data
-        self.__next = None
-        self.__prev = None
 
-    def set_next(self, obj):
-        self.__next = obj
-
-    def set_prev(self, obj):
-        self.__prev = obj
-
-    def get_next(self):
-        return self.__next
-
-    def get_prev(self):
-        return self.__prev
-
-    def set_data(self, data):
-        self.__data = data
-
-    def get_data(self):
-        return self.__data
 
 lst = LinkedList()
 lst.add_obj(ObjList("данные 1"))
