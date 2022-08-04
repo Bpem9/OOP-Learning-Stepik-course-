bbdc37f4cc3630329afbd20dad11bc0fca32ba03
Bpem9 <itimeq@gmail.com>
2022-08-04 13:11:05 +0300

    Completed (by videolesson) exercise

---
+++ 2.Incapsulation/property/DecisionTree.py
@@ -0,0 +1,50 @@
+class DecisionTree:
+
+    @classmethod
+    def predict(cls, root, x):
+        obj = root
+        while obj:
+            obj_next = cls.get_next(obj, x)
+            if obj_next is None:
+                break
+            obj = obj_next
+        return obj.value
+
+    @classmethod
+    def get_next(cls, obj, x):
+        if x[obj.indx] == 1:
+            return obj.left
+        return obj.right
+
+    @classmethod
+    def add_obj(cls, obj, node=None, left=True):
+        if node:
+            if left:
+                node.left = obj
+            else:
+                node.right = obj
+        return obj
+
+
+class TreeObj:
+    def __init__(self, indx, value=None):
+        self.indx = indx
+        self.value = value
+        self.__left = None
+        self.__right = None
+
+    @property
+    def left(self):
+        return self.__left
+
+    @left.setter
+    def left(self, left):
+        self.__left = left
+
+    @property
+    def right(self):
+        return self.__right
+
+    @right.setter
+    def right(self, right):
+        self.__right = right
